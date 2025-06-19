b6158f164bb7d213d34c8c5c6333afcd4f7fa0eb
abhishek.chowdhury <abhishekchowdhury963@gmail.com>
2025-06-20 01:31:37 +0530

    Restructured folder structure

--- terraform/docs/provider.tf
+++
@@ -1,7 +0,0 @@
-# Configure the AWS provider
-provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
-  profile                  = "default"
-}
-
